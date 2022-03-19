9f5b7a91370aed6b1178b2d160733a0bf0055e41
rameshreddy1996 <96303154+rameshreddy1996@users.noreply.github.com>
2022-03-20 04:25:03 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -6,13 +6,13 @@
 sudo yum install git -y
 #Download maven
 cd /opt/
-sudo wget https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.4-bin.tar.gz
+sudo wget https://dlcdn.apache.org/maven/maven-3/3.8.4/binaries/apache-maven-3.8.5-bin.tar.gz
 # extract
-sudo tar -xvzf apache-maven-3.8.4-bin.tar.gz
+sudo tar -xvzf apache-maven-3.8.5-bin.tar.gz
 # rename
-sudo mv apache-maven-3.8.4 maven
+sudo mv apache-maven-3.8.5 maven
 # remove gz
-sudo rm -f apache-maven-3.8.4-bin.tar.gz
+sudo rm -f apache-maven-3.8.5-bin.tar.gz
 #check maven
 cd opt/maven/bin/mvn/mvn
 ./mvn
