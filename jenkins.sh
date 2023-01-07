37a2a55ded826ea5ca59d270658979bfd937858f
rameshreddy1996 <96303154+rameshreddy1996@users.noreply.github.com>
2023-01-07 08:32:21 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -8,3 +8,12 @@ yum install java-11-openjdk-devel -y
 yum install jenkins -y 
 systemctl start jenkins
 systemctl enable jenkins
+---------------###jenkins instaltion####--------------latest 2023
+sudo yum install epel-release -y
+sudo yum install java-11-openjdk -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo --no-check-certificate
+sudo rpm --import http://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install jenkins -y
+
+----------------------####change port number in jenkins#######----------------
+sudo vi /lib/systemd/system/jenkins.service
